07135f02aecd98780af3480f9358387be94a2e52
22969541+Aurelius333@users.noreply.github.com <22969541+Aurelius333@users.noreply.github.com>
2020-03-24 15:03:33 -0400

    more stuff

--- src/thing.ts
+++ src/thing.ts
@@ -6,7 +6,7 @@ import * as t from "@babel/types";
 import prettier from "prettier/standalone";
 import prettierTypeScript from "prettier/parser-typescript";
 import code from "./code";
-import traverse from "@babel/traverse";
+import traverse, { NodePath } from "@babel/traverse";
 
 const opts: { plugins: ["typescript"]; sourceType: "unambiguous" } = {
   plugins: ["typescript"],
@@ -14,39 +14,45 @@ const opts: { plugins: ["typescript"]; sourceType: "unambiguous" } = {
 };
 
 const p = parse(code, opts);
-// const buildingThing = p.program.body.find(
-//   (thing): thing is t.VariableDeclaration =>
-//     t.isVariableDeclaration(thing) &&
-//     t.isNewExpression(thing.declarations[0].init) &&
-//     t.isIdentifier(thing.declarations[0].init.callee, { name: "Building" })
-// );
-// console.log(buildingThing);
-// traverse(p, {
-//   VariableDeclaration(path) {
-//     if (
-//       t.isNewExpression(path.node.declarations[0].init) &&
-//       t.isIdentifier(path.node.declarations[0].init.callee, {
-//         name: "Building",
-//       })
-//     ) {
-//       console.log("hi");
-//       console.log(path.node);
-//     }
-//   },
-// });
-traverse(p, {
-  NewExpression(path) {
-    console.log(path.node);
-    if (
-      t.isIdentifier(path.node.callee, { name: "Hallway" }) &&
-      path.node.loc?.start.line === 53
-    ) {
-      path.node.callee.name = "sdf";
-    }
-  },
-});
-const newExpr = parseExpression("new Room('a')");
-console.log(newExpr);
+let myRoomArray: NodePath<t.ArrayExpression> | undefined = undefined;
+export function findHallwayWithLineNumber(lineNumber: number) {
+  traverse(p, {
+    NewExpression(path) {
+      // // console.log(path.node);
+      if (
+        t.isIdentifier(path.node.callee, { name: "Hallway" }) &&
+        path.node.loc?.start.line === lineNumber &&
+        path.node.arguments.length > 0 &&
+        t.isArrayExpression(path.node.arguments[0]) &&
+        path.node.arguments[0].elements.every((a): a is t.NewExpression =>
+          t.isNewExpression(a)
+        )
+      ) {
+        path.node.callee.name = "sdf";
+        myRoomArray = path.get("arguments.0") as NodePath<t.ArrayExpression>;
+      }
+    },
+  });
+}
+findHallwayWithLineNumber(53);
+function insertRoom(n: number) {
+  if (myRoomArray != null) {
+    // console.log("a");
+    // console.log(myRoomArray);
+    // console.log(myRoomArray.get("elements"));
+    // console.log(myRoomArray.get(`elements.${n}`));
+    // console.log("b");
+    // (myRoomArray.get(`elements[${n - 1}]`) as NodePath<
+    //   t.NewExpression
+    // >).insertAfter(parseExpression("new Room('a')"));
+    myRoomArray.node.elements.splice(n, 0, parseExpression("new Room('z')"));
+    // console.log(myRoomArray.node);
+    // console.log("c");
+    // console.log(p.program);
+  }
+}
+insertRoom(2);
+// console.log(myRoomArray);
 // buildingThing.declarations[0].init
 const generated = generate(p, {
   retainLines: true,
@@ -57,5 +63,5 @@ const formatted = prettier.format(generated, {
   plugins: [prettierTypeScript],
 });
 
-console.log(p);
+// console.log(p);
 console.log(formatted);
